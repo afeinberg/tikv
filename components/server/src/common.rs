5cf2fbebe1a2821004f68f4c47efdcb1fbb7e57d
Alex Feinberg <alex@strlen.net>
2024-04-03 15:14:44 -0700

    Remove dependency of engine_traits on pd.
    Signed-off-by: Alex Feinberg <alex@strlen.net>

--- components/server/src/common.rs
+++ components/server/src/common.rs
@@ -715,7 +715,11 @@ impl KvEngineBuilder for HybridEngine<RocksEngine, RangeCacheMemoryEngine> {
         let mut memory_engine = RangeCacheMemoryEngine::new(std::time::Duration::from_secs(180));
         memory_engine.set_disk_engine(disk_engine.clone());
         if let Some(pd_client) = pd_client.as_ref() {
-            memory_engine.set_pd_client(pd_client.clone())
+            memory_engine.start_hint_service(
+                <RangeCacheMemoryEngine as RangeCacheEngine>::RangeHintService::from(
+                    pd_client.clone(),
+                ),
+            )
         }
         HybridEngine::new(disk_engine, memory_engine)
     }
